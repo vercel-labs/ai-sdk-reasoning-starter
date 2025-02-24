45eb8758167e398d4134dbfa09ff29f2a4bee585
Jared Palmer <jared@jaredpalmer.com>
2025-02-24 12:38:07 -0500

    Improve layout

---
+++ components/deploy-button.tsx
@@ -0,0 +1,8 @@
+export const DeployButton = () => (
+  <a
+    href={`https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-reasoning%2Ftree%2Fmain&env=ANTHROPIC_API_KEY&envDescription=Anthropic%20API%20key&envLink=https%3A%2F%2Fconsole.anthropic.com%2F`}
+    target="_blank"
+  >
+    <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+  </a>
+);
